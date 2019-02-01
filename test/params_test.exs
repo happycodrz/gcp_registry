6010a333f67fc86890f69bab37bacadc75515414
Roman Heinrich <roman.heinrich@gmail.com>
2019-02-01 15:25:58 +0100

    parsing urls with empty images possible

--- test/params_test.exs
+++ test/params_test.exs
@@ -37,17 +37,36 @@ defmodule GcpRegistry.URLParserTest do
       assert Params.from_url(url) |> Map.get(:projectid) == "project1"
       assert Params.from_url(url) |> Map.get(:image) == ""
     end
+
+    test "parses gcr.io/myproject/myimage:mytag1" do
+      url = "gcr.io/myproject/myimage:mytag1"
+
+      assert Params.from_url(url) |> Map.get(:hostname) == "gcr.io"
+      assert Params.from_url(url) |> Map.get(:projectid) == "myproject"
+      assert Params.from_url(url) |> Map.get(:image) == "myimage:mytag1"
+    end
   end
 
   describe "to_api_url" do
-    test "works" do
+    test "works with existing image" do
       params = %Params{
         hostname: "gcr.io",
         projectid: "proj1",
         image: "company1/repo1"
       }
 
-      url = "https://gcr.io/v2/proj1/company1/repo1"
+      url = "https://gcr.io/v2/proj1/company1/repo1/tags/list"
+      assert Params.to_api_url(params) == url
+    end
+
+    test "works with empty image" do
+      params = %Params{
+        hostname: "gcr.io",
+        projectid: "proj1",
+        image: ""
+      }
+
+      url = "https://gcr.io/v2/proj1/tags/list"
       assert Params.to_api_url(params) == url
     end
   end
